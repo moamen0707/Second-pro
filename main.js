c461be5aebd94a64604d47c5609ca7f6e03708ee
moamen0707 <moamenm99@gmail.com>
2024-08-27 17:49:47 +0300

    Upload

---
+++ main.js
@@ -0,0 +1,46 @@
+/*const  btn1=document.querySelector('.dark')
+const  btn2=document.querySelector('.light')
+const body=document.querySelector('body')
+
+
+function changetodarkstyle() {
+    body.style.background='black'
+    body.style.color='#fff'
+    
+}
+
+function changeToLightStyle(){
+
+    body.style.background='#fff'
+    body.style.color='green'
+}
+
+btn1.addEventListener('click',changetodarkstyle )
+btn2.addEventListener('click',changeToLightStyle)*/
+
+/*let students=["ali","moamen","ahmed"]
+students.push( "khaled" )
+students.unshift("mai")
+students.shift()
+students.pop()
+console.table(students)*/
+/*
+let emp =["ahmed0","ali","naser","ahmed0","aliya","naserrr"]
+for (let i = 0; i < emp.length; i++) {
+    console.log(emp(i))
+    
+}
+*/
+let salary1=[300,100,200,400]
+for (let index = 0; index < salary1.length; index++) {
+    const element = [salary1[index]*2]
+    
+    for (let x = 0; x < salary1.length; x++) {
+        console.log(element)
+        
+    }
+    
+}
+
+
+    
